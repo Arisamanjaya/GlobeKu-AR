d5620b84810cbcc0354619ef8de4e93d18fa1391
Arisudana Samanjaya <104663881+Arisamanjaya@users.noreply.github.com>
2024-10-15 00:53:55 +0800

    Initial Commit

---
+++ ProtoGlobeKu/ProtoGlobeKuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoGlobeKuApp.swift
+//  ProtoGlobeKu
+//
+//  Created by I Gede Arisudana Samanjaya on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ProtoGlobeKuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProtoGlobeKuTests/ProtoGlobeKuTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoGlobeKuTests.swift
+//  ProtoGlobeKuTests
+//
+//  Created by I Gede Arisudana Samanjaya on 15/10/24.
+//
+
+import Testing
+@testable import ProtoGlobeKu
+
+struct ProtoGlobeKuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
