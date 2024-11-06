e539b2fc713bfc5075fdbd38129db04f4b2eecf8
Arisudana Samanjaya <104663881+Arisamanjaya@users.noreply.github.com>
2024-11-06 15:46:41 +0800

    First Commit

---
+++ ProtoGlobeKu/Feature Targets/CustomARView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomARView.swift
+//  ProtoGlobeKu
+//
+//  Created by I Gede Arisudana Samanjaya on 15/10/24.
+//
+
+import Foundation
---
+++ ProtoGlobeKu/Feature Targets/CustomARViewRepresentable.swift
@@ -0,0 +1,8 @@
+//
+//  CustomARViewRepresentable.swift
+//  ProtoGlobeKu
+//
+//  Created by I Gede Arisudana Samanjaya on 15/10/24.
+//
+
+import Foundation
